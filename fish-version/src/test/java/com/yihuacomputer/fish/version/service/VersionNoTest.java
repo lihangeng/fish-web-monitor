a520a1e762918ddc3f11b12c932931d419b7f27e
xuxigang <xuxigang@yihuacomputer.com>
2015-10-09 16:11:37 +0800

    add ignore ，删除 the user manaal.doc和增加versionNOtest

--- fish-version/src/test/java/com/yihuacomputer/fish/version/service/VersionNoTest.java
+++ fish-version/src/test/java/com/yihuacomputer/fish/version/service/VersionNoTest.java
@@ -3,6 +3,9 @@ package com.yihuacomputer.fish.version.service;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Test;
+
+import com.yihuacomputer.common.util.StringUtils;
 import com.yihuacomputer.fish.api.version.VersionNo;
 
 public class VersionNoTest {
@@ -18,5 +21,23 @@ public class VersionNoTest {
     
     private boolean bigger(String one,String two){
         return new VersionNo(one).isBiggerThan(new VersionNo(two));
+    }
+    
+    @Test
+    public void testVersionNO(){
+    	System.out.println(generateVersionStr("1.0.0"));
+    	//00000001 00000000 00000000 000000-1
+    	//versionNo.getRevision()的值为-1，所以VersionNo的实现由瑕疵;
+    }
+    
+    private String generateVersionStr(String version){
+    	VersionNo versionNo = new VersionNo(version);
+		StringBuffer versionNoSb = new StringBuffer();
+		versionNoSb.append(StringUtils.preZeroStr(String.valueOf(versionNo.getMajor()), 8)).
+		append(StringUtils.preZeroStr(String.valueOf(versionNo.getMinor()), 8)).
+		append(StringUtils.preZeroStr(String.valueOf(versionNo.getIncremental()), 8)).
+		append(StringUtils.preZeroStr(String.valueOf(versionNo.getRevision()), 8));
+		
+		return versionNoSb.toString();
     }
 }
