30aa985a0a7497eb276dfef84056df29fb0bc088
guoqiang <guoqiang@yihuacompugter.com>
2015-10-28 14:27:42 +0800

    增加功能图表的颜色可控制

--- fish-web-monitor/src/main/webapp/app/view/monitor/charts/View.js
+++ fish-web-monitor/src/main/webapp/app/view/monitor/charts/View.js
@@ -41,9 +41,11 @@ Ext.define('Eway.view.monitor.charts.View', {
 		    },
 		    items:[{
 		    	title:Eway.locale.monitor.summary.allSummary,//'概况',
+		    	itemId:'DonutChartsSummary',
 	            stores:Ext.create("Eway.store.monitor.charts.DonutChartsSummary")
 		    },{
 		    	title:Eway.locale.monitor.summary.appSummary,//'Run概况',
+		    	itemId:'DonutChartsRunSummary',
 	            stores:Ext.create("Eway.store.monitor.charts.DonutChartsRunSummary")
 		    },{
 		    	xtype : 'monitor_device_grid',
@@ -64,14 +66,97 @@ Ext.define('Eway.view.monitor.charts.View', {
 	            }]
 		    },{
 		    	title:Eway.locale.monitor.summary.modSummary,//'Mod概况',
+		    	itemId:'DonutChartsModSummary',
 	            stores:Ext.create("Eway.store.monitor.charts.DonutChartsModSummary")
 		    },{
 		    	title:Eway.locale.monitor.summary.boxSummary,//'Box概况',
+		    	itemId:'DonutChartsBoxSummary',
 	            stores:Ext.create("Eway.store.monitor.charts.DonutChartsBoxSummary")
 		    },{
 		    	title:Eway.locale.monitor.summary.netSummary,//'网络概况',
+		    	itemId:'DonutChartsNetSummary',
 	            stores:Ext.create("Eway.store.monitor.charts.DonutChartsNetSummary")
-		    }]
+		    }],
+		    listeners:{
+		    	beforeactivate:function( _this, eOpts ){
+		    		var item1 = _this.getComponent("DonutChartsNetSummary");
+		    		item1.down("polar").getStore().load({
+        			 	scope: this,
+        			    callback: function(records, operation, success) {
+        			    	if(success){
+        			    		var dount = this.getComponent("DonutChartsNetSummary");
+        			    		var colors = new Array();
+        			    		Ext.Array.forEach(records,function(item,index,allItems){
+        			    			colors.push(item.data.color);
+        			    			dount.down("polar").legend.all.elements[index].childNodes[0].style.backgroundColor=item.data.color;
+        			    		},this);
+        			    		dount.down("polar series[type='pie']").setColors(colors);
+        			    	}
+        			    }
+            		});
+		    		var item2 = _this.getComponent("DonutChartsSummary");
+		    		item2.down("polar").getStore().load({
+        			 	scope: this,
+        			    callback: function(records, operation, success) {
+        			    	if(success){
+        			    		var dount = this.getComponent("DonutChartsSummary");
+        			    		var colors = new Array();
+        			    		Ext.Array.forEach(records,function(item,index,allItems){
+        			    			colors.push(item.data.color);	
+        			    			dount.down("polar").legend.all.elements[index].childNodes[0].style.backgroundColor=item.data.color;
+        			    		},this);
+        			    		dount.down("polar series[type='pie']").setColors(colors);
+        			    	}
+        			    }
+            		});
+		    		var item3 = _this.getComponent("DonutChartsModSummary");
+		    		item3.down("polar").getStore().load({
+        			 	scope: this,
+        			    callback: function(records, operation, success) {
+        			    	if(success){
+        			    		var dount = this.getComponent("DonutChartsModSummary");
+        			    		var colors = new Array();
+        			    		Ext.Array.forEach(records,function(item,index,allItems){
+        			    			colors.push(item.data.color);	
+        			    			dount.down("polar").legend.all.elements[index].childNodes[0].style.backgroundColor=item.data.color;
+        			    		},this);
+        			    		dount.down("polar series[type='pie']").setColors(colors);
+        			    	}
+        			    }
+            		});
+		    		var item4 = _this.getComponent("DonutChartsRunSummary");
+		    		item4.down("polar").getStore().load({
+        			 	scope: this,
+        			    callback: function(records, operation, success) {
+        			    	if(success){
+        			    		var dount = this.getComponent("DonutChartsRunSummary");
+        			    		var colors = new Array();
+        			    		Ext.Array.forEach(records,function(item,index,allItems){
+        			    			colors.push(item.data.color);
+        			    			dount.down("polar").legend.all.elements[index].childNodes[0].style.backgroundColor=item.data.color;
+        			    		},this);
+        			    		dount.down("polar series[type='pie']").setColors(colors);
+        			    	}
+        			    }
+            		});
+		    		var item5 = _this.getComponent("DonutChartsBoxSummary");
+		    		item5.down("polar").getStore().load({
+        			 	scope: this,
+        			    callback: function(records, operation, success) {
+        			    	if(success){
+        			    		var dount = this.getComponent("DonutChartsBoxSummary");
+        			    		var colors = new Array();
+        			    		Ext.Array.forEach(records,function(item,index,allItems){
+        			    			colors.push(item.data.color);
+        			    			dount.down("polar").legend.all.elements[index].childNodes[0].style.backgroundColor=item.data.color;
+        			    		},this);
+        			    		dount.down("polar series[type='pie']").setColors(colors);
+        			    	}
+        			    }
+            		});
+		    		
+		    	}
+		    }
 
 		});
 
