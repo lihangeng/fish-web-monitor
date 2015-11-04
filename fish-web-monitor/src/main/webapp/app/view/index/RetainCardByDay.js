f49b2aab148edace90f728931d3b91667e83a2b3
xuxigang <xuxigang@yihuacomputer.com>
2015-11-04 17:23:14 +0800

    调整首页的顺序和高度

--- fish-web-monitor/src/main/webapp/app/view/index/RetainCardByDay.js
+++ fish-web-monitor/src/main/webapp/app/view/index/RetainCardByDay.js
@@ -28,8 +28,8 @@ Ext.define('Eway.view.index.RetainCardByDay', {
         me.items = [{
             xtype: 'cartesian',
             width: '100%',
-            height: 300,
-           interactions: {
+            height: 260,
+            interactions: {
                 type: 'itemhighlight',
                 zoomOnPanGesture: false
             },
--- fish-web-monitor/src/main/webapp/app/view/index/VersionDistributePie.js
+++ fish-web-monitor/src/main/webapp/app/view/index/VersionDistributePie.js
@@ -85,7 +85,7 @@ Ext.define('Eway.view.index.VersionDistributePie', {
             xtype: 'polar',
             theme: 'default-gradients',
             width: '100%',
-            height: 300,
+            height: 260,
             store: me.myDataStore,
             insetPadding: 30,
             innerPadding: 10,
