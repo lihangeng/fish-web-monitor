2d844f91d4651f9afedbcff665ee771ac9fc6a28
guoqiang <guoqiang@yihuacompter.com>
2015-11-23 15:00:25 +0800

    银行机构调整，右边机构树移除

--- fish-web-monitor/src/main/webapp/app/view/person/bankOrg/View.js
+++ fish-web-monitor/src/main/webapp/app/view/person/bankOrg/View.js
@@ -15,22 +15,22 @@ Ext.define('Eway.view.person.bankOrg.View', {
 		var store = Ext.create('Eway.store.person.organization.BankOrganizationTree');
 		Ext.apply(this, {
 			items: [{
-				region: 'east',
-				title : Eway.view.person.bankOrg.orgNavi,
-				header: false,
-				xtype : 'treepanel',
-				width : 200,
-				minWidth:200,
-				maxWidth:300,
-				border : true,
-				//padding : '10 0 10 0',
-				rootVisible : true,
-				lines : true,
-				store : store,
-				collapsible: false,
-             	//collapseMode: 'mini',
-             	split:false
-			}, {
+//				region: 'east',
+//				title : Eway.view.person.bankOrg.orgNavi,
+//				header: false,
+//				xtype : 'treepanel',
+//				width : 200,
+//				minWidth:200,
+//				maxWidth:300,
+//				border : true,
+//				//padding : '10 0 10 0',
+//				rootVisible : true,
+//				lines : true,
+//				store : store,
+//				collapsible: false,
+//             	//collapseMode: 'mini',
+//             	split:false
+//			}, {
 				region: 'center',
 				xtype: 'panel',
 				layout: 'border',
@@ -44,14 +44,14 @@ Ext.define('Eway.view.person.bankOrg.View', {
 			}],
 			listeners : {
 				activate : function(panel) {
-					if (!panel.isLoad) {
+//					if (!panel.isLoad) {
+//
+//						// 第一次进来不需要重新加载信息
+//						panel.isLoad = true;
+//						return;
+//					}
 
-						// 第一次进来不需要重新加载信息
-						panel.isLoad = true;
-						return;
-					}
-
-					panel.down('treepanel').getStore().load();
+					panel.down('bank_organization_grid').getStore().load();
 				}
 			}
 		});
