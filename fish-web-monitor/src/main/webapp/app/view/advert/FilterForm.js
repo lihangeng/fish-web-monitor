20d690bebb7bb27832013760fb7818b72116ba4a
guoqiang <guoqiang@yihuacompter.com>
2015-11-24 16:14:56 +0800

    广告页面时间控件约束处理

--- fish-web-monitor/src/main/webapp/app/view/advert/FilterForm.js
+++ fish-web-monitor/src/main/webapp/app/view/advert/FilterForm.js
@@ -5,6 +5,35 @@ Ext.define('Eway.view.advert.FilterForm', {
 	requires : [ 'Ext.ux.form.DateTimeField' ],
 	height : 35,
 	initComponent : function() {
+		Ext.apply(Ext.form.field.VTypes, {
+			cardInfoDateRange : function(val, field) {
+				var beginDate = null, // 开始日期
+				beginDateCmp = null, // 开始日期组件
+				endDate = null, // 结束日期
+				enddateCmp = null, // 结束日期组件
+				validStatus = true;// 验证状态
+				if (field.dateRange) {
+					// 获取开始时间
+					if (!Ext.isEmpty(field.dateRange.begin)) {
+						var filterForm = Ext.ComponentQuery.query('advert_filterForm')[0];
+						beginDateCmp = filterForm.down('field[name="createdTimeStart"]');
+						beginDate = beginDateCmp.getValue();
+
+					}
+					// 获取结束时间
+					if (!Ext.isEmpty(field.dateRange.end)) {
+						var filterForm = Ext.ComponentQuery.query('advert_filterForm')[0];
+						enddateCmp = filterForm.down('field[name="createdTimeEnd"]');
+						endDate = enddateCmp.getValue();
+					}
+				}
+				if (!Ext.isEmpty(beginDate) && !Ext.isEmpty(endDate)) {
+					validStatus = beginDate <= endDate;
+				}
+				return validStatus;
+			},
+			cardInfoDateRangeText : EwayLocale.tip.dateReSelect
+		});
 		var me = this;
 		Ext.apply(this, {
 			layout : 'column',
@@ -31,7 +60,21 @@ Ext.define('Eway.view.advert.FilterForm', {
 					name : 'createdTimeStart',
 					editable : false,
 					format : 'Y-m-d H:i:s',
-					width : 335
+					width : 335,
+					value : Ext.Date.add(new Date(), Ext.Date.DAY, -2),
+					 dateRange : {
+					 begin : 'createdTimeStart',
+					 end : 'createdTimeEnd'
+					 },
+					 vtype : 'cardInfoDateRange',
+					 msgTarget : 'side',
+					 labelAlign : 'right',
+					 listeners: {
+					 	change : function(){
+					 		var endDate = this.up('form').down('field[name="createdTimeEnd"]');
+					 		endDate.clearInvalid();
+					 	}
+					 }
 				} ]
 			}, {
 				columnWidth : .3,
@@ -42,7 +85,21 @@ Ext.define('Eway.view.advert.FilterForm', {
 					labelWidth:130,
 					editable : false,
 					format : 'Y-m-d H:i:s',
-					width : 310
+					width : 310,
+					value : new Date(),
+					dateRange : {
+						begin : 'createdTimeStart',
+						end : 'createdTimeEnd'
+					},
+					vtype : 'cardInfoDateRange',
+					msgTarget : 'side',
+					labelAlign : 'right',
+					listeners : {
+						change : function(){
+							var beginDate = this.up('form').down('field[name="createdTimeStart"]');
+							beginDate.clearInvalid();
+						}
+					}
 				} ]
 			} ]
 		});
