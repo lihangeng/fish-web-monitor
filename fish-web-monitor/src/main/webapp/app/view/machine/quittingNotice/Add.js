692a7b4700123381e4f004349718cd06775c5445
guoqiang <guoqiang@yihuacompter.com>
2015-11-27 11:39:56 +0800

    页面展示效果微调

--- fish-web-monitor/src/main/webapp/app/view/machine/quittingNotice/Add.js
+++ fish-web-monitor/src/main/webapp/app/view/machine/quittingNotice/Add.js
@@ -49,7 +49,7 @@ Ext.define('Eway.view.machine.quittingNotice.Add', {
 				selectOnFocus : true,
 				defaults: {
 					width: 400,
-					labelWidth: 80,
+					labelWidth: 110,
 					labelAlign: 'right',
 					msgTarget : 'side'
 				},
