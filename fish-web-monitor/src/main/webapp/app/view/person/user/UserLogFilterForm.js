ab5c132f20354277c5827ba5bb448e5ef0b2c673
guoqiang <guoqiang@yihuacompter.com>
2015-12-15 14:19:40 +0800

    时间范围过滤

--- fish-web-monitor/src/main/webapp/app/view/person/user/UserLogFilterForm.js
+++ fish-web-monitor/src/main/webapp/app/view/person/user/UserLogFilterForm.js
@@ -13,35 +13,6 @@ Ext.define('Eway.view.person.user.UserLogFilterForm', {
 
 	initComponent: function() {
 		var me = this;
-
-		Ext.apply(Ext.form.field.VTypes, {
-			userLogDateRange : function(val, field) {
-				var beginDate = null, // 开始日期
-				endDate = null, // 结束日期
-				validStatus = true;// 验证状态
-				if (field.dateRange) {
-					// 获取开始时间
-					if (!Ext.isEmpty(field.dateRange.begin)) {
-						var filterForm = Ext.ComponentQuery.query('userLogFilterForm')[0];
-						var beginDateCmp = filterForm.down('field[name="operTimeStart"]');
-						beginDate = beginDateCmp.getValue();
-
-					}
-					// 获取结束时间
-					if (!Ext.isEmpty(field.dateRange.end)) {
-						var filterForm = Ext.ComponentQuery.query('userLogFilterForm')[0];
-						var enddateCmp = filterForm.down('field[name="operTimeEnd"]');
-						endDate = enddateCmp.getValue();
-					}
-				}
-				if (!Ext.isEmpty(beginDate) && !Ext.isEmpty(endDate)) {
-					validStatus = beginDate <= endDate;
-				}
-				return validStatus;
-			},
-			userLogDateRangeText : EwayLocale.vtype.endDateGtBenginDate
-		});
-
 		Ext.apply(this, {
 			items : [{
 				columnWidth : .5,
@@ -54,18 +25,18 @@ Ext.define('Eway.view.person.user.UserLogFilterForm', {
 					labelWidth:140,
 					editable: false,
 					value:Ext.Date.parse(Ext.Date.format(new Date(), 'Y-m-d') + " 00:00:00","Y-m-d H:i:s"),
-					dateRange : {
-						 begin : 'operTimeStart',
-						 end : 'operTimeEnd'
-					 },
-					 vtype : 'userLogDateRange',
 					 msgTarget : 'side',
 					 labelAlign : 'right',
 					 listeners: {
 					 	change : function(){
 					 		var endDate = this.up('form').down('field[name="operTimeEnd"]');
-					 		endDate.clearInvalid();
-					 	}
+					 		endDate.setMinValue(this.getValue());
+					 	},
+						render:function(){
+							var endDate = this.up('form').down('field[name="operTimeEnd"]');
+
+							this.setMaxValue(endDate.getValue());
+						}
 					 }
 				}]
 			},
@@ -79,17 +50,16 @@ Ext.define('Eway.view.person.user.UserLogFilterForm', {
 					format : 'Y-m-d H:i:s',
 					editable: false,
 					value:Ext.Date.parse(Ext.Date.format(new Date(), 'Y-m-d') + " 23:59:59","Y-m-d H:i:s"),
-					dateRange : {
-						begin : 'operTimeStart',
-						end : 'operTimeEnd'
-					},
-					vtype : 'userLogDateRange',
 					msgTarget : 'side',
 					labelAlign : 'right',
 					listeners : {
 						change : function(){
 							var beginDate = this.up('form').down('field[name="operTimeStart"]');
-							beginDate.clearInvalid();
+							beginDate.setMaxValue(this.getValue());
+						},
+						render:function(){
+							var beginDate = this.up('form').down('field[name="operTimeStart"]');
+							this.setMinValue(beginDate.getValue());
 						}
 					}
 
