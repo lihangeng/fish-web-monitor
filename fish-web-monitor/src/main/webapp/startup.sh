39f148d012f0999dc4a7c76f21f02c5f4f2815ba
guoqiang <guoqiang@yihuacompter.com>
2015-12-21 13:39:27 +0800

    liunx启动停用WebApp

--- fish-web-monitor/src/main/webapp/startup.sh
+++ fish-web-monitor/src/main/webapp/startup.sh
@@ -1,7 +1,20 @@
 #!/bin/sh
-APP_PATH="/home/yihua/ATMVS"
+APP_PATH=`pwd`
 export APP_PATH
-
+WEBPIDFILE=WEBPID.ini
+WEB_PIDFILE=$APP_PATH/$WEBPIDFILE
+#是否存在INI文件，不存在直接启动
+if [ -f "$WEB_PIDFILE" ];then
+#存在INI文件,读取PID
+WEBPID=`awk -F "=" '{if($1=="WEB_PID")print $2}' $WEBPIDFILE`
+#检查进程中是否存在相应的PID
+checkReuslt=`ps -o pid |grep $WEBPID |awk '{if($1=='$WEBPID')print "ok"}'`
+#存在退出启动操作
+if [[ $checkReuslt = "ok" ]];then
+        echo "Web App is started"
+        exit;
+fi
+fi
 APP_LIB_PATH="$APP_PATH/WEB-INF/lib"
 export APP_LIB_PATH
 
@@ -17,3 +30,4 @@ export JAVA_OPTS
 
 #java  $JAVA_OPTS -classpath $CLASSPATH -Dfile.encoding=utf-8 com.yihuacomputer.fish.web.FishConsole
 nohup java  $JAVA_OPTS -classpath $CLASSPATH -Dfile.encoding=utf-8 com.yihuacomputer.fish.web.FishConsole >/dev/null 2>1 &
+echo "Web App is Starting"
