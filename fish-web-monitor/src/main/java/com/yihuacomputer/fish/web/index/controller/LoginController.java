d9682b7a4341fc6446b164cf2b9268976da9f501
guoqiang <guoqiang@yihuacompter.com>
2015-12-28 14:00:24 +0800

    删除无用织入的IpermissionService

--- fish-web-monitor/src/main/java/com/yihuacomputer/fish/web/index/controller/LoginController.java
+++ fish-web-monitor/src/main/java/com/yihuacomputer/fish/web/index/controller/LoginController.java
@@ -27,7 +27,6 @@ import com.yihuacomputer.common.annotation.MethodNameDescrible;
 import com.yihuacomputer.common.exception.AppException;
 import com.yihuacomputer.common.util.MsgDigestAlgorithm;
 import com.yihuacomputer.fish.api.permission.IPermission;
-import com.yihuacomputer.fish.api.permission.IPermissionService;
 import com.yihuacomputer.fish.api.person.IOrganization;
 import com.yihuacomputer.fish.api.person.IUser;
 import com.yihuacomputer.fish.api.person.IUserService;
@@ -224,9 +223,6 @@ public class LoginController {
 		}
 		return forms;
 	}
-
-	@Autowired
-	private IPermissionService service;
 
 	private boolean hasChildMenu(IPermission self,List<IPermission> permissions){
 		for(IPermission permission : permissions){
