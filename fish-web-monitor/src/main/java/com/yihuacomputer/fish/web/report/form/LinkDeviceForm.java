3e69436cb39a3beb2c1eddebc35e9112ffad7a54
xuxigang <xuxigang@yihuacomputer.com>
2016-01-02 14:25:41 +0800

    删除无用的配置

--- fish-web-monitor/src/main/java/com/yihuacomputer/fish/web/report/form/LinkDeviceForm.java
+++ fish-web-monitor/src/main/java/com/yihuacomputer/fish/web/report/form/LinkDeviceForm.java
@@ -73,71 +73,9 @@ public class LinkDeviceForm {
 	private String serial;
 
 	/**
-	 * 设备购买日期 format(yyyy-MM-dd)
-	 */
-	private String buyDate;
-
-	/**
 	 * 设备安装日期 format(yyyy-MM-dd)
 	 */
 	private String installDate;
-
-	/**
-	 * 设备启用日期 format(yyyy-MM-dd)
-	 */
-	private String startDate;
-
-	/**
-	 * 设备信用日期 format(yyyy-MM-dd)
-	 */
-	private String stopDate;
-
-	/**
-	 * 每日开机时间 format(HH:mm:ss)
-	 */
-	private String openTime;
-
-	private String openTimeHour;
-
-	private String openTimeMinute;
-
-	private String openTimeSecond;
-
-	/**
-	 * 每日关机时间 format(HH:mm:ss)
-	 */
-	private String closeTime;
-
-	private String closeTimeHour;
-
-	private String closeTimeMinute;
-
-	private String closeTimeSecond;
-
-	/**
-	 * 保修到期日期 format(yyyy-MM-dd)
-	 */
-	private String expireDate;
-
-	/**
-	 * 设备巡检周期
-	 */
-	private int patrolPeriod;
-
-	/**
-	 * 设备关注程序
-	 */
-	private String careLevel;
-
-	/**
-	 * 上次巡检日期
-	 */
-	private String lastPmDate;
-
-	/**
-	 * 巡检到期日期
-	 */
-	private String expirePmDate;
 
 	/**
 	 * 非现金标志
@@ -153,71 +91,6 @@ public class LinkDeviceForm {
 	 * 运营商
 	 */
 	private String carrier;
-
-	/**
-	 * 加钞机构
-	 */
-	private String moneyOrg;
-
-	/**
-	 * 入账成本. 单位：元
-	 */
-	private double price;
-
-	/**
-	 * 折旧年限. 单位：年
-	 */
-	private double depreciationLife;
-
-	/**
-	 * 装修费用
-	 */
-	private double decoration;
-
-	/**
-	 * 装修摊销年限.　单位：年
-	 */
-	private double decorationCost;
-
-	/**
-	 * 物业租赁费. 单位：元/月
-	 */
-	private double governanceRent;
-
-	/**
-	 * 物业管理费用.　单位：元/月
-	 */
-	private double governanceCost;
-
-	/**
-	 * 通讯线路费用．单位：元/月
-	 */
-	private double netCost;
-
-	/**
-	 * 电费．单位：元/月
-	 */
-	private double powerCost;
-
-	/**
-	 * 加钞维护费用．单位：元/次
-	 */
-	private double moneyCost;
-
-	/**
-	 * 资金成本利率
-	 */
-	private String costInterest;
-
-	/**
-	 * atmc软件
-	 */
-	private String atmcSoft;
-
-	/**
-	 * 厂商sp类型
-	 */
-	private String sp;
 
 	/**
 	 * 网络类型
@@ -249,58 +122,8 @@ public class LinkDeviceForm {
 		setAddress(device.getAddress());
 		setCashboxLimit(device.getCashboxLimit());
 		setAwayFlag(device.getAwayFlag() == null ? null : String.valueOf(device.getAwayFlag().getId()));
-		// setCareLevel(device.getCareLevel() == null ? null :
-		// String.valueOf(device.getCareLevel().getId()));
-		// setCashType(device.getCashType() == null ? null :
-		// String.valueOf(device.getCashType().getId()));
 		setSetupType(device.getSetupType() == null ? null : String.valueOf(device.getSetupType().getId()));
 		setWorkType(device.getWorkType() == null ? null : String.valueOf(device.getWorkType().getId()));
-		// setAtmcSoft(device.getAtmcSoft());
-		// setSp(device.getSp());
-		/*
-		 * if (device.getSerial() != null) { IDeviceExtend deviceExtend =
-		 * device.getDeviceExtend();
-		 * 
-		 * setNetType(deviceExtend.getNetType() == null ? null :
-		 * String.valueOf(deviceExtend.getNetType().getId()));
-		 * 
-		 * setBuyDate(nullString(deviceExtend.getBuyDate()));
-		 * setExpireDate(nullString(deviceExtend.getExpireDate()));
-		 * setExpirePmDate(nullString(deviceExtend.getExpirePmDate()));
-		 * setInstallDate(nullString(deviceExtend.getInstallDate()));
-		 * setLastPmDate(nullString(deviceExtend.getLastPmDate()));
-		 * setStartDate(nullString(deviceExtend.getStartDate()));
-		 * setStopDate(nullString(deviceExtend.getStopDate()));
-		 * 
-		 * // setOpenTime(deviceExtend.getOpenTime().toString()); //
-		 * setCloseTime(deviceExtend.getCloseTime().toString());
-		 * 
-		 * if (deviceExtend.getOpenTime() != null &&
-		 * deviceExtend.getOpenTime().matches("\\d{2}:\\d{2}:\\d{2}")) {
-		 * String[] openTimes = deviceExtend.getOpenTime().split(":");
-		 * setOpenTimeHour(openTimes[0]); setOpenTimeMinute(openTimes[1]);
-		 * setOpenTimeSecond(openTimes[2]); } if (deviceExtend.getCloseTime() !=
-		 * null && deviceExtend.getCloseTime().matches("\\d{2}:\\d{2}:\\d{2}"))
-		 * { String[] closeTimes = deviceExtend.getCloseTime().split(":");
-		 * setCloseTimeHour(closeTimes[0]); setCloseTimeMinute(closeTimes[1]);
-		 * setCloseTimeSecond(closeTimes[2]); }
-		 * 
-		 * setCarrier(deviceExtend.getCarrier());
-		 * setCostInterest(deviceExtend.getCostInterest());
-		 * setDecoration(deviceExtend.getDecoration());
-		 * setDecorationCost(deviceExtend.getDecorationCost());
-		 * setDepreciationLife(deviceExtend.getDepreciationLife());
-		 * setGovernanceCost(deviceExtend.getGovernanceCost());
-		 * setGovernanceRent(deviceExtend.getGovernanceRent());
-		 * setMoneyCost(deviceExtend.getMoneyCost());
-		 * setMoneyOrg(deviceExtend.getMoneyOrg());
-		 * setNetCost(deviceExtend.getNetCost());
-		 * setTerminalId(deviceExtend.getTerminalId());
-		 * setPatrolPeriod(deviceExtend.getPatrolPeriod());
-		 * setPowerCost(deviceExtend.getPowerCost());
-		 * setPrice(deviceExtend.getPrice());
-		 * setSerial(deviceExtend.getSerial()); }
-		 */
 		if (device.getDevService() != null) {
 			setDevServiceName(device.getDevService().getName());
 			setDevServiceId(device.getDevService().getGuid());
@@ -469,16 +292,6 @@ public class LinkDeviceForm {
 		this.devVendorName = devVendorName;
 	}
 
-	// public long getDevCatalogId()
-	// {
-	// return devCatalogId;
-	// }
-
-	// public void setDevCatalogId(long devCatalogId)
-	// {
-	// this.devCatalogId = devCatalogId;
-	// }
-
 	public String getDevCatalogName() {
 		return devCatalogName;
 	}
@@ -495,92 +308,12 @@ public class LinkDeviceForm {
 		this.serial = serial;
 	}
 
-	public String getBuyDate() {
-		return buyDate;
-	}
-
-	public void setBuyDate(String buyDate) {
-		this.buyDate = buyDate;
-	}
-
 	public String getInstallDate() {
 		return installDate;
 	}
 
 	public void setInstallDate(String installDate) {
 		this.installDate = installDate;
-	}
-
-	public String getStartDate() {
-		return startDate;
-	}
-
-	public void setStartDate(String startDate) {
-		this.startDate = startDate;
-	}
-
-	public String getStopDate() {
-		return stopDate;
-	}
-
-	public void setStopDate(String stopDate) {
-		this.stopDate = stopDate;
-	}
-
-	public String getOpenTime() {
-		return openTime;
-	}
-
-	public void setOpenTime(String openTime) {
-		this.openTime = openTime;
-	}
-
-	public String getCloseTime() {
-		return closeTime;
-	}
-
-	public void setCloseTime(String closeTime) {
-		this.closeTime = closeTime;
-	}
-
-	public String getExpireDate() {
-		return expireDate;
-	}
-
-	public void setExpireDate(String expireDate) {
-		this.expireDate = expireDate;
-	}
-
-	public int getPatrolPeriod() {
-		return patrolPeriod;
-	}
-
-	public void setPatrolPeriod(int patrolPeriod) {
-		this.patrolPeriod = patrolPeriod;
-	}
-
-	public String getCareLevel() {
-		return careLevel;
-	}
-
-	public void setCareLevel(String careLevel) {
-		this.careLevel = careLevel;
-	}
-
-	public String getLastPmDate() {
-		return lastPmDate;
-	}
-
-	public void setLastPmDate(String lastPmDate) {
-		this.lastPmDate = lastPmDate;
-	}
-
-	public String getExpirePmDate() {
-		return expirePmDate;
-	}
-
-	public void setExpirePmDate(String expirePmDate) {
-		this.expirePmDate = expirePmDate;
 	}
 
 	public String getCashType() {
@@ -607,110 +340,6 @@ public class LinkDeviceForm {
 		this.carrier = carrier;
 	}
 
-	public String getMoneyOrg() {
-		return moneyOrg;
-	}
-
-	public void setMoneyOrg(String moneyOrg) {
-		this.moneyOrg = moneyOrg;
-	}
-
-	public double getPrice() {
-		return price;
-	}
-
-	public void setPrice(double price) {
-		this.price = price;
-	}
-
-	public double getDepreciationLife() {
-		return depreciationLife;
-	}
-
-	public void setDepreciationLife(double depreciationLife) {
-		this.depreciationLife = depreciationLife;
-	}
-
-	public double getDecoration() {
-		return decoration;
-	}
-
-	public void setDecoration(double decoration) {
-		this.decoration = decoration;
-	}
-
-	public double getDecorationCost() {
-		return decorationCost;
-	}
-
-	public void setDecorationCost(double decorationCost) {
-		this.decorationCost = decorationCost;
-	}
-
-	public double getGovernanceRent() {
-		return governanceRent;
-	}
-
-	public void setGovernanceRent(double governanceRent) {
-		this.governanceRent = governanceRent;
-	}
-
-	public double getGovernanceCost() {
-		return governanceCost;
-	}
-
-	public void setGovernanceCost(double governanceCost) {
-		this.governanceCost = governanceCost;
-	}
-
-	public double getNetCost() {
-		return netCost;
-	}
-
-	public void setNetCost(double netCost) {
-		this.netCost = netCost;
-	}
-
-	public double getPowerCost() {
-		return powerCost;
-	}
-
-	public void setPowerCost(double powerCost) {
-		this.powerCost = powerCost;
-	}
-
-	public double getMoneyCost() {
-		return moneyCost;
-	}
-
-	public void setMoneyCost(double moneyCost) {
-		this.moneyCost = moneyCost;
-	}
-
-	public String getCostInterest() {
-		return costInterest;
-	}
-
-	public void setCostInterest(String costInterest) {
-		this.costInterest = costInterest;
-	}
-
-	public String getAtmcSoft() {
-		return atmcSoft;
-	}
-
-	public void setAtmcSoft(String atmcSoft) {
-		this.atmcSoft = atmcSoft;
-	}
-
-	public String getSp() {
-		return sp;
-	}
-
-	public void setSp(String sp) {
-		this.sp = sp;
-	}
-
 	public String getNetType() {
 		return netType;
 	}
@@ -727,62 +356,6 @@ public class LinkDeviceForm {
 		this.awayFlag = awayFlag;
 	}
 
-	public String getWorkType() {
-		return workType;
-	}
-
-	public void setWorkType(String workType) {
-		this.workType = workType;
-	}
-
-	public String getOpenTimeHour() {
-		return openTimeHour;
-	}
-
-	public void setOpenTimeHour(String openTimeHour) {
-		this.openTimeHour = openTimeHour;
-	}
-
-	public String getOpenTimeMinute() {
-		return openTimeMinute;
-	}
-
-	public void setOpenTimeMinute(String openTimeMinute) {
-		this.openTimeMinute = openTimeMinute;
-	}
-
-	public String getOpenTimeSecond() {
-		return openTimeSecond;
-	}
-
-	public void setOpenTimeSecond(String openTimeSecond) {
-		this.openTimeSecond = openTimeSecond;
-	}
-
-	public String getCloseTimeHour() {
-		return closeTimeHour;
-	}
-
-	public void setCloseTimeHour(String closeTimeHour) {
-		this.closeTimeHour = closeTimeHour;
-	}
-
-	public String getCloseTimeMinute() {
-		return closeTimeMinute;
-	}
-
-	public void setCloseTimeMinute(String closeTimeMinute) {
-		this.closeTimeMinute = closeTimeMinute;
-	}
-
-	public String getCloseTimeSecond() {
-		return closeTimeSecond;
-	}
-
-	public void setCloseTimeSecond(String closeTimeSecond) {
-		this.closeTimeSecond = closeTimeSecond;
-	}
-
 	@SuppressWarnings("unchecked")
 	private <T> T nullObject(String value, Class<T> targetClass) {
 		T resultObj = null;
@@ -796,4 +369,13 @@ public class LinkDeviceForm {
 		}
 		return resultObj;
 	}
+
+	public String getWorkType() {
+		return workType;
+	}
+
+	public void setWorkType(String workType) {
+		this.workType = workType;
+	}
+
 }
