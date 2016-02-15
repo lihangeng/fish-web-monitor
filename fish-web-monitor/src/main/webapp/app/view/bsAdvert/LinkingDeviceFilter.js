a1378e22f3a679eac22fe6337a218568bee962fd
panxin <panxin@yihuacomputer.com>
2016-02-15 09:15:23 +0800

    广告组流程修改

--- fish-web-monitor/src/main/webapp/app/view/bsAdvert/LinkingDeviceFilter.js
+++ fish-web-monitor/src/main/webapp/app/view/bsAdvert/LinkingDeviceFilter.js
@@ -1,7 +1,9 @@
 Ext.define('Eway.view.bsAdvert.LinkingDeviceFilter', {
 	extend : 'Eway.view.base.FilterForm',
 	alias : 'widget.bsAdvert_linkingDeviceFilter',
-	height : 40,
+	requires : [ 'Eway.lib.Util','Eway.model.version.SelectableDevice',
+				 'Eway.view.field.device.DeviceAtmType','Eway.view.common.OrgComboOrgTree', 'Eway.view.field.atmType.DeviceAtmCatalogComboBox' ],
+	layout : 'column',
 	defaults : {
 		border : false
 	},
@@ -12,10 +14,60 @@ Ext.define('Eway.view.bsAdvert.LinkingDeviceFilter', {
 			},
 			items : [ {
 				columnWidth : .5,
-				xtype : 'textfield',
-				fieldLabel : EwayLocale.commen.terminalId,
-				name : 'terminalId'
-			} ]
+				defaults : {
+					labelAlign : 'right',
+					labelWidth : 40
+				},
+				items : [ {
+					xtype : 'common_orgComboOrgTree',
+					fieldLabel : EwayLocale.refs.orgName,
+					emptyText : EwayLocale.combox.select,
+					name : 'orgName',
+					hiddenValue : 'orgId',
+					editable : true,
+					width:200,
+					filters : '{"type" : "0"}',
+					rootVisible : ewayUser.getOrgType() != "" && ewayUser.getOrgType() == '0' ? true : false
+				},  {
+					style : 'padding-top:0px',
+					xtype : 'hiddenfield',
+					name : 'orgId'
+				},  {
+					style : 'padding-top:0px',
+					xtype : 'hiddenfield',
+					name : 'flag',
+					value : '0'
+				},{
+					xtype : 'textfield',
+					fieldLabel : 'IP',
+					name : 'ip',
+					width:200,
+					regex : /^(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])$/,
+					regexText : EwayLocale.vtype.ip
+				}]
+			}, {
+				columnWidth : .5,
+				defaults : {
+					labelAlign : 'right',
+					labelWidth : 80
+				},
+				items : [  {
+					xtype : 'textfield',
+					fieldLabel :EwayLocale.commen.terminalId,
+					width:240,
+					name : 'terminalId'
+				},{
+					style : 'padding-top:0px',
+					xtype : 'hiddenfield',
+					name : 'planId'
+				},{
+					xtype : 'field_device_deviceatmtype',
+					name : 'devType',
+					width:240,
+					hiddenName : 'devType'
+				} ]
+			}
+			]
 		});
 		this.callParent(arguments);
 	}
