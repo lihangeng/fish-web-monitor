75f1d5c6a056760e561642423828cc6016c67de2
guoqiang <guoqiang@yihuacompter.com>
2016-02-19 16:43:31 +0800

    访问广告组代码添加注释

--- fish-web-monitor/src/main/java/com/yihuacomputer/fish/web/atm/AdvertUrlController.java
+++ fish-web-monitor/src/main/java/com/yihuacomputer/fish/web/atm/AdvertUrlController.java
@@ -52,12 +52,14 @@ public class AdvertUrlController {
 		ModelMap result = new ModelMap();
 		String callbackName = (String)request.getParameter("jsoncallback");
 		String terminalId = request.getParameter("terminalId");
-		String path = "1";
+		String path = "1 1";
 		if(terminalId !=null){
+			//根据设备获取设备归属机构信息
 			IDevice device=deviceService.get(terminalId);
 			if(device!=null){
 				String orgFlag=device.getOrganization().getOrgFlag();
 				String orgGuids[] = orgFlag.split("-");
+				//分割机构Flag,并且倒序查找(从子机构向父机构查找广告组路径)
 				for(int index=orgGuids.length-1;index>=0;index--){
 					IAdvertGroup group = getGroupPath(Long.parseLong(orgGuids[index]),device.getId());
 					if(group!=null){
@@ -79,17 +81,19 @@ public class AdvertUrlController {
 	 */
 	private IAdvertGroup getGroupPath(long orgId,long deviceId){
 		IAdvertGroup defaultGroup = null;
-		List<IAdvertGroup> list=advertGroupService.list(orgId);
+		//查找当前机构的广告组
+		List<IAdvertGroup> list = advertGroupService.list(orgId);
 		if(list.isEmpty()){
 			return null;
 		}else{
 			List<Long> groupIdList = new ArrayList<Long>();
 			//如果存在默认组，则将默认组缓存；不存在关联设备时候，直接返回默认组,存在关联的广告组，直接返回广告组
 			for(IAdvertGroup group:list){
+				//默认广告组必须存在激活的广告，才能返回默认广告组的路径
 				if(group.getGroupType().equals(GroupType.DEFAULT)){
 					IFilter filter  = new Filter();
 					filter.eq("groupId", group.getId());
-					filter.ne("bsAdvertStatus", BsAdvertStatus.UNACTIVE);
+					filter.eq("bsAdvertStatus", BsAdvertStatus.ACTIVED);
 					List<IBsAdvert> advertList = bsAdvertService.list(filter);
 					if(advertList.size()>0){
 						defaultGroup = group;
@@ -100,6 +104,7 @@ public class AdvertUrlController {
 				}
 			}
 			IAdvertGroupDeviceRelation advertGroupDeviceRelation=advertGroupDeviceRelationService.getGroup(deviceId,groupIdList);
+			//如果存在关联的普通广告组则用普通广告组的路径
 			if(null!=advertGroupDeviceRelation){
 				defaultGroup = advertGroupService.getById(advertGroupDeviceRelation.getGroupId());
 			}
