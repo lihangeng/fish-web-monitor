11e3db51e450c49a58f1e999aa16a3ec16e42c56
zhengnan <zhengnan@yihuacomputer.com>
2016-03-24 15:01:47 +0800

    基本完成功能需求。

--- fish-web-monitor/src/main/java/com/yihuacomputer/fish/web/parameter/controller/ClassifyController.java
+++ fish-web-monitor/src/main/java/com/yihuacomputer/fish/web/parameter/controller/ClassifyController.java
@@ -7,6 +7,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,7 +21,9 @@ import com.yihuacomputer.common.IPageResult;
 import com.yihuacomputer.common.filter.Filter;
 import com.yihuacomputer.fish.api.parameter.IClassify;
 import com.yihuacomputer.fish.api.parameter.IClassifyService;
+import com.yihuacomputer.fish.api.parameter.IElement;
 import com.yihuacomputer.fish.web.parameter.form.ClassifyForm;
+import com.yihuacomputer.fish.web.parameter.form.ElementForm;
 
 @Controller
 @RequestMapping("/parameter/classify")
@@ -35,11 +39,57 @@ public class ClassifyController {
 		logger.info(String.format("search template : start = %s ,limt = %s ",start, limit));
 		IFilter filter = request2filter(request);
 		ModelMap result = new ModelMap();
-		IPageResult<IClassify> pageResult = classifyService.page(start, limit,filter);
+		IPageResult<IClassify> pageResult = classifyService.page(start, limit, filter);
 		result.addAttribute(FishConstant.SUCCESS, true);
 		result.addAttribute(FishConstant.TOTAL, pageResult.getTotal());
 		logger.info("classify size:" + pageResult.getTotal());
 		result.addAttribute(FishConstant.DATA,ClassifyForm.convert(pageResult.list()));
+		return result;
+	}
+
+	@RequestMapping(method=RequestMethod.POST)
+	public @ResponseBody
+	ModelMap add(@RequestBody ClassifyForm request){
+		logger.info("add classify");
+		ModelMap result=new ModelMap();
+		IClassify classify =classifyService.make();
+		request.translate(classify);
+		classifyService.add(classify);
+		result.put(FishConstant.SUCCESS, true);
+		result.addAttribute(FishConstant.DATA, new ClassifyForm(classify));
+		return result;
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public @ResponseBody
+	ModelMap delete(@PathVariable long id) {
+		logger.info(" delete classify: classify.id = " + id);
+		ModelMap result = new ModelMap();
+		try {
+			if(id == 1){
+				result.addAttribute(FishConstant.ERROR_MSG, false);
+				return result;
+			} else {
+				classifyService.remove(id);
+				result.addAttribute(FishConstant.SUCCESS, true);
+			}
+		} catch (Exception ex) {
+			result.addAttribute(FishConstant.SUCCESS, false);
+		}
+		return result;
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public @ResponseBody
+	ModelMap update(@PathVariable long id, @RequestBody ClassifyForm request) {
+		logger.info("update classify: classify.id = " + id);
+		ModelMap result = new ModelMap();
+		IClassify classify = classifyService.get(id);
+		request.translate(classify);
+		classify.setId(id);
+		classifyService.update(classify);
+		result.addAttribute(FishConstant.SUCCESS, true);
+		result.addAttribute(FishConstant.DATA, request);
 		return result;
 	}
 
