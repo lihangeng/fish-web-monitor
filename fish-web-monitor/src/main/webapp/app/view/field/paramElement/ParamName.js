b745fc583b482549b28f71a7f62b36b28d5fe7f5
lihangeng <lihangeng@yihuacomputer.com>
2016-04-13 10:49:50 +0800

    参数元数据管理参数名改为必输字段

--- fish-web-monitor/src/main/webapp/app/view/field/paramElement/ParamName.js
+++ fish-web-monitor/src/main/webapp/app/view/field/paramElement/ParamName.js
@@ -3,7 +3,7 @@ Ext.define('Eway.view.field.paramElement.ParamName', {
 	extend: 'Ext.form.field.Text',
 	alias: 'widget.field_paramElement_ParamName',
 
-	fieldLabel : EwayLocale.param.element.paramName,
+	fieldLabel : '<font color="red">*</font>'+EwayLocale.param.element.paramName,
 	name : 'paramName'
 
 });
