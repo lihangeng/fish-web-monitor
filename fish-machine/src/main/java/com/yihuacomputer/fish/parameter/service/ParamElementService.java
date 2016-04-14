c21937a17c2225d7c8ee438d6b29f0ff58ccb8d8
lihangeng <lihangeng@yihuacomputer.com>
2016-04-14 11:52:23 +0800

    参数管理功能测试问题修改

--- fish-machine/src/main/java/com/yihuacomputer/fish/parameter/service/ParamElementService.java
+++ fish-machine/src/main/java/com/yihuacomputer/fish/parameter/service/ParamElementService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.yihuacomputer.common.IFilter;
 import com.yihuacomputer.common.IPageResult;
 import com.yihuacomputer.domain.dao.IGenericDao;
+import com.yihuacomputer.fish.api.parameter.IParamClassify;
 import com.yihuacomputer.fish.api.parameter.IParamElement;
 import com.yihuacomputer.fish.api.parameter.IParamElementService;
 import com.yihuacomputer.fish.parameter.entity.ParamElement;
@@ -86,6 +87,13 @@ public class ParamElementService implements IParamElementService {
 		hql.append("select t from ParamElement t");
 		List<IParamElement> result = dao.findByHQL(hql.toString());
 		return result;
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public List<IParamElement> listByClassify(IParamClassify paramClassify) {
+
+		return dao.findByHQL("from ParamElement paramElement where paramElement.paramClassify.id = ?", paramClassify.getId());
 	}
 
 }
