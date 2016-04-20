2d9b7e632192b190764fa72b07465b2247bec7ad
lvxiuzhuan <lvxiuchuan@yihuacomputer.com>
2016-04-20 09:32:51 +0800

    前台字符解析报错解决

--- fish-web-monitor/src/main/webapp/app/view/parameter/devParameter/View.js
+++ fish-web-monitor/src/main/webapp/app/view/parameter/devParameter/View.js
@@ -16,7 +16,7 @@ Ext.define('Eway.view.parameter.devParameter.View', {
 						region: 'west',
 						layout:'border',
 						width:'35%',
-						collapsible:true,
+//						split:true,
 						items:[{
 							region:'north',
 							xtype:'parameter_devParameter_devFilterForm'
@@ -31,10 +31,9 @@ Ext.define('Eway.view.parameter.devParameter.View', {
 						xtype:'tabpanel',
 						tabPosition : 'left',
 						headerPosition: 'left',
-						plain:true,
+						plain:false,
 					    activeTab: 0,
 					    tabRotation:0
-					    
 					}],
 					listeners: {
 				       'beforerender':function( _this, eOpts){
@@ -42,7 +41,7 @@ Ext.define('Eway.view.parameter.devParameter.View', {
 				   			method:'GET',
 				   			url:'api/parameter/devParameter/getAppData',
 				   			success:function(response){
-				   				if(response.responseText !=null){
+				   				if(response.responseText){
 				   					var appData=Ext.decode(response.responseText);
 					   				for(var i=0;i<appData.total;i++){
 					   					var tab=Ext.create('Eway.view.parameter.devParameter.ParamView',{appType:appData.data[i].id});
