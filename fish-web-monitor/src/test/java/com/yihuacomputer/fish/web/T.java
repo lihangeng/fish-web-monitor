8638b682c69a66d2598cce2cb2a56ab2e9d6deb1
shaoxuan <shaoxuan@yihuacomputer.com>
2016-05-17 15:52:51 +0800

    测试

--- fish-web-monitor/src/test/java/com/yihuacomputer/fish/web/T.java
+++ fish-web-monitor/src/test/java/com/yihuacomputer/fish/web/T.java
@@ -15,7 +15,7 @@ public class T {
 
 //		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Shanghai"));
 //		TimeZone.setDefault(TimeZone.getTimeZone("GMT"));
-		System.out.print(new Date(1384544188203l));
+		System.out.print(new Date(138454418820300l));
 		for(int i =0 ;i < 1 ;i ++){
 			long s = System.currentTimeMillis();
 			System.out.println(s);
