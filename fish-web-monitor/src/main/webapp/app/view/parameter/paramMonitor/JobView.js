110603d2ee614db384add1c6c3889ef3e369414e
lvxiuzhuan <lvxiuchuan@yihuacomputer.com>
2016-06-12 15:49:39 +0800

    参数下发监控界面优化

--- fish-web-monitor/src/main/webapp/app/view/parameter/paramMonitor/JobView.js
+++ fish-web-monitor/src/main/webapp/app/view/parameter/paramMonitor/JobView.js
@@ -1,11 +1,12 @@
 Ext.define('Eway.view.parameter.paramMonitor.JobView', {
-	extend : 'Eway.view.base.Panel',
+	extend : 'Ext.panel.Panel',
 	alias : 'widget.parameter_paramMonitor_JobView',
 
 	requires : [ 'Eway.view.parameter.paramMonitor.JobFilterForm',
 			     'Eway.view.parameter.paramMonitor.JobGrid' ],
 	
 	layout:'border',
+	closable:false,
 	initComponent:function(){
 		
 		Ext.apply(this,{
--- fish-web-monitor/src/main/webapp/app/view/parameter/paramMonitor/TaskView.js
+++ fish-web-monitor/src/main/webapp/app/view/parameter/paramMonitor/TaskView.js
@@ -9,9 +9,31 @@ Ext.define('Eway.view.parameter.paramMonitor.TaskView', {
 		jobId : 0
 	},
 	layout : 'border',
+	closable:false,
 	name : 'paramMonitorDetails',
 	initComponent : function() {
 		Ext.apply(this, {
+			layout : 'border',
+			initRegion : true,
+			border : true,
+			weight : 15,
+			tbar : [ {
+				text : EwayLocale.report.faultRateReport.back,
+				glyph : 0xf122,
+				action : 'back',
+				tooltip : EwayLocale.report.faultRateReport.back,
+				code : 'back'
+			}, "->", {
+				glyph : 0xf060,
+				action : 'pref',
+				tooltip : EwayLocale.report.faultRateReport.preType,
+				code : 'pref'
+			}, {
+				glyph : 0xf061,
+				action : 'next',
+				tooltip : EwayLocale.report.faultRateReport.nextType,
+				code : 'next'
+			} ],
 			items : [ {
 				region : 'north',
 				xtype : 'parameter_paramMonitor_TaskFilterForm'
