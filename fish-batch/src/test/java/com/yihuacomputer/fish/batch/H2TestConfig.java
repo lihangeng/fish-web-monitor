25c77eb884bca8ee1762394964e222b6240f7655
xuxigang <xuxigang@yihuacomputer.com>
2016-08-22 15:09:55 +0800

    batch 测试代码调整（依赖包的版本不对spring-core导致无法测试）

--- fish-batch/src/test/java/com/yihuacomputer/fish/batch/H2TestConfig.java
+++ fish-batch/src/test/java/com/yihuacomputer/fish/batch/H2TestConfig.java
@@ -1,14 +1,11 @@
 package com.yihuacomputer.fish.batch;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.ImportResource;
 
-import com.yihuacomputer.domain.DomainModule;
-
 @Configuration
-@Import({DomainModule.class})
-@ImportResource(value = "classpath:/com/yihuacomputer/fish/batch/batch-core.xml")
+//@Import({DomainModule.class})
+@ImportResource(value = "classpath:/com/yihuacomputer/fish/batch/batch_core.xml")
 public class H2TestConfig {
 
 }
