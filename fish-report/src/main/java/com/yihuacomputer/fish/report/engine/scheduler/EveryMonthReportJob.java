bdc64d89c6f2a2feaa5dabf81e24d7e4b60c36bf
xuxigang <xuxigang@yihuacomputer.com>
2016-09-02 11:36:41 +0800

    代码优化

--- fish-report/src/main/java/com/yihuacomputer/fish/report/engine/scheduler/EveryMonthReportJob.java
+++ fish-report/src/main/java/com/yihuacomputer/fish/report/engine/scheduler/EveryMonthReportJob.java
@@ -12,6 +12,7 @@ import com.yihuacomputer.fish.api.monitor.volume.IMonthDailyTradingVolumeService
 import com.yihuacomputer.fish.api.report.device.etl.IDeviceCatalogSummaryMonthService;
 import com.yihuacomputer.fish.api.report.device.etl.IDeviceTypeSummaryMonthService;
 import com.yihuacomputer.fish.api.report.device.etl.IRetainCardEtlService;
+import com.yihuacomputer.fish.api.report.engine.IPdfReportService;
 import com.yihuacomputer.fish.api.report.fault.IEveryMonthFaultCountService;
 import com.yihuacomputer.fish.api.report.fault.etl.IFaultEtlService;
 import com.yihuacomputer.fish.api.report.openRate.etl.IAvgOpenRateEtlService;
@@ -68,39 +69,100 @@ public class EveryMonthReportJob {
 	
 	@Autowired
 	private IDeviceTypeSummaryMonthService deviceTypeSummaryMonthService;
+	
+	@Autowired
+	private IPdfReportService pdfReportService;
 
+	/**
+	 * 每月任务
+	 */
 	public void execute(){
-		logger.debug("month data ETL ....");
+		logger.info("starting month data ETL ....");
+		long start = System.currentTimeMillis();
+		executeMonthDevice();
+		executeMonthTrans();
+		executeMonthOpenRate();
+		executeMonthFault();
+		executeMonthPdfReport();
+		logger.info(String.format("month data ETL finished. SPEND [%s]ms",System.currentTimeMillis() - start));
+	}
+	
+	/**
+	 * 生成月综合分析报告
+	 */
+	private void executeMonthPdfReport() {
+		long start = System.currentTimeMillis();
+		Date lastMonth =DateUtils.getLastMonth();
+		pdfReportService.generateMonthPDF((int)DateUtils.getLongYM(lastMonth));
+		logger.info(String.format("executeMonthPdfReport SPEND [%s]ms",System.currentTimeMillis() - start));
+	}
+
+	/**
+	 * 故障相关月统计
+	 */
+	private void executeMonthFault() {
+		long start = System.currentTimeMillis();
+		//1.（2016年2季度增加）计算设备故障率 CASE_FAULT_MONTH
+		String yestoday = DateUtils.getLastMonthShortDates();
+		everyMonthFaultCountService.extractMonthFault(yestoday);
+		
+		//2.每月故障类型统计(2016年3季度需求综合报告之故障信息汇总)
+		Date lastMonth =DateUtils.getLastMonth();
+		faultEtlService.extractStatusByMonth(lastMonth);
+		faultEtlService.extractFaultClassifyByMonth(lastMonth);
+		faultEtlService.extractDurationByMonth(lastMonth);
+		logger.info(String.format("executeMonthFault SPEND [%s]ms",System.currentTimeMillis() - start));
+	}
+
+	/**
+	 * 开机率相关月统计
+	 */
+	private void executeMonthOpenRate(){
+		long start = System.currentTimeMillis();
+		Date lastMonth =DateUtils.getLastMonth();
+		//1.每月开机率统计(2016年3季度需求综合报告之开机率信息汇总)
+		avgOpenRateEtlService.extractByMonth(lastMonth);
+		//2.每月设备开机率统计(2016年3季度需求综合报告之开机率信息汇总)
+		deviceOpenRateEtlService.extractByMonth(lastMonth);
+		//3.每月设备型号开机率统计(2016年3季度需求综合报告之开机率信息汇总)
+		deviceTypeOpenRateEtlService.extractByMonth(lastMonth);
+		//4.每月机构开机率统计(2016年3季度需求综合报告之开机率信息汇总)
+		orgOpenRateEtlService.extractByMonth(lastMonth);
+		logger.info(String.format("executeMonthOpenRate SPEND [%s]ms",System.currentTimeMillis() - start));
+
+	}
+	/**
+	 * 交易相关月统计
+	 */
+	private void executeMonthTrans() {
+		long start = System.currentTimeMillis();
 		//1.每月交易统计（2016年2季度增加）ATMC_TRANSACTION_MONTHS
 		String yestoday = DateUtils.getLastMonthShortDates();
 		transactionMonthsService.extractDate(yestoday);
-		//2.（2016年2季度增加）计算设备故障率 CASE_FAULT_MONTH
-		everyMonthFaultCountService.extractMonthFault(yestoday);
-		//3.每台设备日均交易统计(加钞计划使用)
+		//2.每台设备日均交易统计(加钞计划使用)
 		String curdate = DateUtils.getDateShort(new Date());
 		monthDailyTradingVolumeService.generalMonthDailyTradingVolumeByDate(curdate);
-		
-		//0.设备类统计
+		//3.每月交易类型统计(2016年3季度需求综合报告之交易信息汇总)
+		Date lastMonth =DateUtils.getLastMonth();
+		transTypeEtlService.extractByMonth(lastMonth);
+		logger.info(String.format("executeMonthTrans SPEND [%s]ms",System.currentTimeMillis() - start));
+
+	}
+
+	/**
+	 * 设备相关月统计
+	 */
+	private void executeMonthDevice() {
+		long start = System.currentTimeMillis();
+		//1.设备类型统计
 		Date dateNow = new Date();
 		deviceCatalogSummaryMonthService.loadBaseData(dateNow);
+		//2.设备型号统计
 		deviceTypeSummaryMonthService.loadBaseData(dateNow);
-		Date date =DateUtils.getDate(DateUtils.getLastDate());
-		//1.每周开机率统计(2016年3季度需求综合报告之开机率信息汇总)
-		avgOpenRateEtlService.extractByMonth(date);
-		//2.每周设备开机率统计(2016年3季度需求综合报告之开机率信息汇总)
-		deviceOpenRateEtlService.extractByMonth(date);
-		//3.每周设备型号开机率统计(2016年3季度需求综合报告之开机率信息汇总)
-		deviceTypeOpenRateEtlService.extractByMonth(date);
-		//4.每周机构开机率统计(2016年3季度需求综合报告之开机率信息汇总)
-		orgOpenRateEtlService.extractByMonth(date);
-		//5.每周交易类型统计(2016年3季度需求综合报告之交易信息汇总)
-		transTypeEtlService.extractByMonth(date);
-		//6.每周吞卡统计(2016年3季度需求综合报告之吞卡信息汇总)
-		retainCardEtlService.extractByMonth(date);
-		//7.每周故障类型统计(2016年3季度需求综合报告之故障信息汇总)
-		faultEtlService.extractStatusByMonth(date);
-		faultEtlService.extractFaultClassifyByMonth(date);
-		faultEtlService.extractDurationByMonth(date);
-		logger.debug("month data ETL finished");
+		//3.每月吞卡统计(2016年3季度需求综合报告之吞卡信息汇总)
+		Date lastMonth =DateUtils.getLastMonth();
+		retainCardEtlService.extractByMonth(lastMonth);
+		logger.info(String.format("executeMonthDevice SPEND [%s]ms",System.currentTimeMillis() - start));
+
 	}	
 }
--- fish-report/src/main/java/com/yihuacomputer/fish/report/engine/scheduler/EveryWeekReportJob.java
+++ fish-report/src/main/java/com/yihuacomputer/fish/report/engine/scheduler/EveryWeekReportJob.java
@@ -11,6 +11,7 @@ import com.yihuacomputer.common.util.DateUtils;
 import com.yihuacomputer.fish.api.report.device.etl.IDeviceCatalogSummaryWeekService;
 import com.yihuacomputer.fish.api.report.device.etl.IDeviceTypeSummaryWeekService;
 import com.yihuacomputer.fish.api.report.device.etl.IRetainCardEtlService;
+import com.yihuacomputer.fish.api.report.engine.IPdfReportService;
 import com.yihuacomputer.fish.api.report.fault.etl.IFaultEtlService;
 import com.yihuacomputer.fish.api.report.openRate.etl.IAvgOpenRateEtlService;
 import com.yihuacomputer.fish.api.report.openRate.etl.IDeviceOpenRateEtlService;
@@ -56,31 +57,88 @@ public class EveryWeekReportJob {
 	@Autowired
 	private IDeviceTypeSummaryWeekService deviceTypeSummaryWeekService;
 	
+	@Autowired
+	private IPdfReportService pdfReportService;
+	
 	/**
 	 * 每周统计
 	 */
 	public void execute(){
-		logger.debug("week data ETL finished");
-		Date date =DateUtils.getDate(DateUtils.getLastDate());
-		//0.设备类统计
-		deviceCatalogSummaryWeekService.loadBaseData(date);
-		deviceTypeSummaryWeekService.loadBaseData(date);
+		logger.info("Week data ETL finished");
+		long start = System.currentTimeMillis();
+		executeWeekDevice();
+		executeWeekOpenRate();
+		executeWeekTrans();
+		executeWeekFault();
+		executeWeekPdfReport();
+		logger.info(String.format("Week data ETL finished,SPEND [%s]ms",System.currentTimeMillis() - start));
+	}
+
+	/**
+	 * 生成周综合分析报告
+	 */
+	private void executeWeekPdfReport() {
+		long start = System.currentTimeMillis();
+		Date lastWeek = DateUtils.getLastWeek();
+		pdfReportService.generateMonthPDF(DateUtils.getWeek(lastWeek).intValue());
+		logger.info(String.format("executeWeekPdfReport SPEND [%s]ms",System.currentTimeMillis() - start));
+	}
+
+	/**
+	 * 故障类相关周统计
+	 */
+	private void executeWeekFault() {
+		long start = System.currentTimeMillis();
+		//1.每周故障类型统计(2016年3季度需求综合报告之故障信息汇总)
+		Date lastWeek = DateUtils.getLastWeek();
+		faultEtlService.extractStatusByWeek(lastWeek);
+		faultEtlService.extractFaultClassifyByWeek(lastWeek);
+		faultEtlService.extractDurationByWeek(lastWeek);
+		logger.info(String.format("executeWeekFault SPEND [%s]ms",System.currentTimeMillis() - start));
+	}
+
+	/**
+	 * 交易相关周统计
+	 */
+	private void executeWeekTrans() {
+		long start = System.currentTimeMillis();
+		//1.每周交易类型统计(2016年3季度需求综合报告之交易信息汇总)
+		Date lastWeek = DateUtils.getLastWeek();
+		transTypeEtlService.extractByWeek(lastWeek);
+		logger.info(String.format("executeWeekTrans SPEND [%s]ms",System.currentTimeMillis() - start));
+	}
+
+	/**
+	 * 开机率相关的周统计
+	 */
+	private void executeWeekOpenRate() {
+		long start = System.currentTimeMillis();
+		Date lastWeek = DateUtils.getLastWeek();
 		//1.每周开机率统计(2016年3季度需求综合报告之开机率信息汇总)
-		avgOpenRateEtlService.extractByWeek(date);
+		avgOpenRateEtlService.extractByWeek(lastWeek);
 		//2.每周设备开机率统计(2016年3季度需求综合报告之开机率信息汇总)
-		deviceOpenRateEtlService.extractByWeek(date);
+		deviceOpenRateEtlService.extractByWeek(lastWeek);
 		//3.每周设备型号开机率统计(2016年3季度需求综合报告之开机率信息汇总)
-		deviceTypeOpenRateEtlService.extractByWeek(date);
+		deviceTypeOpenRateEtlService.extractByWeek(lastWeek);
 		//4.每周机构开机率统计(2016年3季度需求综合报告之开机率信息汇总)
-		orgOpenRateEtlService.extractByWeek(date);
-		//5.每周交易类型统计(2016年3季度需求综合报告之交易信息汇总)
-		transTypeEtlService.extractByWeek(date);
-		//6.每周吞卡统计(2016年3季度需求综合报告之吞卡信息汇总)
-		retainCardEtlService.extractByWeek(date);
-		//7.每周故障类型统计(2016年3季度需求综合报告之故障信息汇总)
-		faultEtlService.extractStatusByWeek(date);
-		faultEtlService.extractFaultClassifyByWeek(date);
-		faultEtlService.extractDurationByWeek(date);
+		orgOpenRateEtlService.extractByWeek(lastWeek);
+		logger.info(String.format("executeWeekOpenRate SPEND [%s]ms",System.currentTimeMillis() - start));
+	}
+
+	/**
+	 * 执行设备基础信息的相关周统计
+	 */
+	private void executeWeekDevice() {
+		long start = System.currentTimeMillis();
+		Date now = new Date();
+		//1.设备类型统计
+		deviceCatalogSummaryWeekService.loadBaseData(now);
+		//2.设备型号统计
+		deviceTypeSummaryWeekService.loadBaseData(now);
+		//3.每周吞卡统计(2016年3季度需求综合报告之吞卡信息汇总)
+		Date lastWeek = DateUtils.getLastWeek();
+		retainCardEtlService.extractByWeek(lastWeek);
+		logger.info(String.format("executeWeekDevice SPEND [%s]ms",System.currentTimeMillis() - start));
 	}
 	
 }
