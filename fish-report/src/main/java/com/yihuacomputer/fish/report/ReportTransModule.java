d57eba09de35b5e6306ba64ebc674e67e466373a
lvxiuzhuan <lvxiuchuan@yihuacomputer.com>
2016-09-02 16:24:20 +0800

    报表数据引入

--- fish-report/src/main/java/com/yihuacomputer/fish/report/ReportTransModule.java
+++ fish-report/src/main/java/com/yihuacomputer/fish/report/ReportTransModule.java
@@ -5,6 +5,16 @@ import org.springframework.context.annotation.Configuration;
 
 import com.yihuacomputer.fish.api.monitor.box.ICashInitPlanInfoService;
 import com.yihuacomputer.fish.api.monitor.volume.IDayTradingVolumeService;
+import com.yihuacomputer.fish.api.report.device.etl.IDeviceCatalogSummaryMonthService;
+import com.yihuacomputer.fish.api.report.device.etl.IDeviceCatalogSummaryWeekService;
+import com.yihuacomputer.fish.api.report.device.etl.IDeviceTypeSummaryMonthService;
+import com.yihuacomputer.fish.api.report.device.etl.IDeviceTypeSummaryWeekService;
+import com.yihuacomputer.fish.api.report.fault.etl.IFaultEtlService;
+import com.yihuacomputer.fish.api.report.openRate.IDayOpenRateService;
+import com.yihuacomputer.fish.api.report.openRate.etl.IAvgOpenRateEtlService;
+import com.yihuacomputer.fish.api.report.openRate.etl.IDeviceOpenRateEtlService;
+import com.yihuacomputer.fish.api.report.openRate.etl.IDeviceTypeOpenRateEtlService;
+import com.yihuacomputer.fish.api.report.openRate.etl.IOrgOpenRateEtlService;
 import com.yihuacomputer.fish.api.report.trans.ICashInRptService;
 import com.yihuacomputer.fish.api.report.trans.ISettlementCashInRptService;
 import com.yihuacomputer.fish.api.report.trans.ISettlementRptService;
@@ -14,6 +24,16 @@ import com.yihuacomputer.fish.api.report.trans.ITransactionMonthsService;
 import com.yihuacomputer.fish.api.report.trans.etl.ITransTypeEtlService;
 import com.yihuacomputer.fish.monitor.entity.volume.DayTradingVolumeService;
 import com.yihuacomputer.fish.monitor.service.CashInitPlanInfoService;
+import com.yihuacomputer.fish.report.service.device.etl.DeviceCatalogSummaryMonthService;
+import com.yihuacomputer.fish.report.service.device.etl.DeviceCatalogSummaryWeekService;
+import com.yihuacomputer.fish.report.service.device.etl.DeviceTypeSummaryMonthService;
+import com.yihuacomputer.fish.report.service.device.etl.DeviceTypeSummaryWeekService;
+import com.yihuacomputer.fish.report.service.fault.etl.FaultEtlService;
+import com.yihuacomputer.fish.report.service.openRate.DayOpenRateService;
+import com.yihuacomputer.fish.report.service.openRate.etl.AvgOpenRateEtlService;
+import com.yihuacomputer.fish.report.service.openRate.etl.DeviceOpenRateEtlService;
+import com.yihuacomputer.fish.report.service.openRate.etl.DeviceTypeOpenRateEtlService;
+import com.yihuacomputer.fish.report.service.openRate.etl.OrgOpenRateEtlService;
 import com.yihuacomputer.fish.report.service.trans.CashInRptService;
 import com.yihuacomputer.fish.report.service.trans.SettlementCashInRptService;
 import com.yihuacomputer.fish.report.service.trans.SettlementRptService;
@@ -76,5 +96,44 @@ public class ReportTransModule {
 	public ICashInitPlanInfoService cashInitPlanInfoService(){
 		return new CashInitPlanInfoService();
 	}
-	
+	@Bean
+	public IDeviceCatalogSummaryMonthService deviceCatalogSummaryMonthService(){
+		return new DeviceCatalogSummaryMonthService();
+	}
+	@Bean
+	public IDeviceCatalogSummaryWeekService deviceCatalogSummaryWeekService(){
+		return new DeviceCatalogSummaryWeekService();
+	}
+	@Bean
+	public IDeviceTypeSummaryMonthService deviceTypeSummaryMonthService(){
+		return new DeviceTypeSummaryMonthService();
+	}
+	@Bean
+	public IDeviceTypeSummaryWeekService deviceTypeSummaryWeekService(){
+		return new DeviceTypeSummaryWeekService();
+	}
+	@Bean
+	public IAvgOpenRateEtlService avgOpenRateEtlService(){
+		return new AvgOpenRateEtlService();
+	}
+	@Bean
+	public IDeviceOpenRateEtlService deviceOpenRateEtlService(){
+		return new DeviceOpenRateEtlService();
+	}
+	@Bean 
+	public IDeviceTypeOpenRateEtlService deviceTypeOpenRateEtlService(){
+		return new DeviceTypeOpenRateEtlService();
+	}
+	@Bean
+	public IOrgOpenRateEtlService orgOpenRateEtlService(){
+		return new OrgOpenRateEtlService();
+	}
+	@Bean
+	public IDayOpenRateService dayOpenRateService(){
+		return new DayOpenRateService();
+	}
+	@Bean
+	public IFaultEtlService faultEtlService(){
+		return new FaultEtlService();
+	}
 }
