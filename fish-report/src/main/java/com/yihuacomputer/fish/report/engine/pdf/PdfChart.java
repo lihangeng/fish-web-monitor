d91dd9421fe7f5d926e105548e40a9a5d85bc0da
lvxiuzhuan <lvxiuchuan@yihuacomputer.com>
2016-09-05 19:47:13 +0800

    折现和柱形图优化

--- fish-report/src/main/java/com/yihuacomputer/fish/report/engine/pdf/PdfChart.java
+++ fish-report/src/main/java/com/yihuacomputer/fish/report/engine/pdf/PdfChart.java
@@ -2,7 +2,11 @@ package com.yihuacomputer.fish.report.engine.pdf;
 
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.JFreeChart;
+import org.jfree.chart.labels.StandardCategoryItemLabelGenerator;
+import org.jfree.chart.plot.CategoryPlot;
 import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.renderer.category.BarRenderer;
+import org.jfree.chart.renderer.category.LineAndShapeRenderer;
 import org.jfree.data.category.DefaultCategoryDataset;
 import org.jfree.data.general.DefaultPieDataset;
 
@@ -22,12 +26,23 @@ public class PdfChart {
 	 * @return
 	 */
 	public static JFreeChart generateLineChart(DefaultCategoryDataset dataset) {
-	return ChartFactory.createLineChart("", "", "", dataset,
-            PlotOrientation.VERTICAL, // 绘制方向
-            true, // 显示图例
-            true, // 采用标准生成器
-            false // 是否生成超链接
-            );
+		JFreeChart chart = ChartFactory.createLineChart("", "", "", dataset,
+	            PlotOrientation.VERTICAL, // 绘制方向
+	            true, // 显示图例
+	            true, // 采用标准生成器
+	            false // 是否生成超链接
+	            );
+		CategoryPlot categoryplot = (CategoryPlot) chart.getPlot();
+	    LineAndShapeRenderer lineandshaperenderer = (LineAndShapeRenderer) categoryplot.getRenderer();
+	    lineandshaperenderer.setBaseShapesVisible(true); // series 点（即数据点）可见
+	    lineandshaperenderer.setBaseLinesVisible(true); // series 点（即数据点）间有连线可见
+	 
+	    // 显示折点数据
+	    lineandshaperenderer
+	            .setBaseItemLabelGenerator(new StandardCategoryItemLabelGenerator());
+	    lineandshaperenderer.setBaseItemLabelsVisible(true);
+
+		return chart;
 	}
 	
 	/**
@@ -38,6 +53,12 @@ public class PdfChart {
 	public static JFreeChart generateBarChart(DefaultCategoryDataset dataSet) {
 		JFreeChart chart = ChartFactory.createBarChart(EMTPY_STR, EMTPY_STR, EMTPY_STR,
 				dataSet, PlotOrientation.VERTICAL, false, true, false);
+		 CategoryPlot categoryPlot = (CategoryPlot) chart.getPlot();
+		  BarRenderer barRenderer = (BarRenderer) categoryPlot.getRenderer();
+		  barRenderer.setIncludeBaseInRange(true);
+		    barRenderer.setBaseItemLabelGenerator(
+		            new StandardCategoryItemLabelGenerator());
+		    barRenderer.setBaseItemLabelsVisible(true);
 		return chart;
 	}
 	
