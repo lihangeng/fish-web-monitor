51513e47b7675cec1cb5df82f2046a745d341c4d
guoqiang <guoqiang@yihuacomputer.com>
2016-09-09 10:13:18 +0800

    增加通知类型

--- fish-web-monitor/src/main/webapp/app/store/case/NotifyWay.js
+++ fish-web-monitor/src/main/webapp/app/store/case/NotifyWay.js
@@ -6,7 +6,9 @@ Ext.define('Eway.store.case.NotifyWay',{
 	fields : ['value','display'],
 	data : [
 	    {'value':'NONE', 'display':EwayLocale.cases.caseFault.none},
-		{'value':'SMS', 'display':EwayLocale.cases.caseFault.message}
+		{'value':'SMS', 'display':EwayLocale.cases.caseFault.message},
+		{'value':'MAIL', 'display':EwayLocale.cases.caseFault.mail},
+		{'value':'BOTH', 'display':EwayLocale.cases.caseFault.both}
 	]
 
 });
